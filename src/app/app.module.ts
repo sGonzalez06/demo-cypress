2fe3468bbe978397c81863873c55d5907355dd5f
sGonzalez06 <113132935+sGonzalez06@users.noreply.github.com>
2025-05-15 02:09:38 -0300

    updates in main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,11 @@ import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { HomepageComponent } from './components/homepage/homepage.component';
-import { ForgotPasswordComponent } from './components/forgot-password/forgot-password.component';
 import { AuthGuard } from './guards/auth.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: '/login', pathMatch: 'full' },
   { path: 'login', component: LoginComponent },
-  { path: 'forgot-password', component: ForgotPasswordComponent },
   { path: 'homepage', component: HomepageComponent, canActivate: [AuthGuard] },
   { path: '**', redirectTo: '/login' }
 ];
@@ -21,8 +19,7 @@ const routes: Routes = [
   declarations: [
     AppComponent,
     LoginComponent,
-    HomepageComponent,
-    ForgotPasswordComponent
+    HomepageComponent
   ],
   imports: [
     BrowserModule,
